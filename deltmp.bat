864f4afe4a94d68325a24e79acb40b5fa49fd20b
Daniel Marschall <info@daniel-marschall.de>
2025-09-10 23:00:39 +0000

    Changed run energy code, and added parameters to Forest.ini
    git-svn-id: https://svn.viathinksoft.com/svn/forest/trunk@33 3f5c83d6-f77d-41c6-9111-d15273089fe5

--- deltmp.bat
+++ deltmp.bat
@@ -3,11 +3,17 @@
 rd /s /q dll_resize\__history
 del dll_resize\*.~*
 del dll_resize\*.dcu
+del dll_resize\*.identcache
+del dll_resize\*.local
 
 rd /s /q mapgen\__history
 del mapgen\*.~*
 del mapgen\*.dcu
+del mapgen\*.identcache
+del mapgen\*.local
 
 rd /s /q cfgread\__history
 del cfgread\*.~*
 del cfgread\*.dcu
+del cfgread\*.identcache
+del cfgread\*.local
